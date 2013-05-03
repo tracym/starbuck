150ebea515c771090d1071471b3dbd496eac6c28
Tracy Malkemes <tuz24919@temple.edu>
2013-05-03 14:00:21 -0400

    removed stolen stackoverflow and used merge instead

--- src/starbuck/core.clj
+++ src/starbuck/core.clj
@@ -15,30 +15,14 @@
   (merge-with #(or %1 %2) lmap
     (empty-unioned-map lmap rmap)))
 
-(defn map-difference 
-  "todo: find out who I stole this from on stack overflow"
-  [m1 m2]
-  (let [ks1 (set (keys m1))
-        ks2 (set (keys m2))
-        ks1-ks2 (difference ks1 ks2)
-        ks2-ks1 (difference ks2 ks1)
-        ks1*ks2 (intersection ks1 ks2)]
-    (merge (select-keys m1 ks1-ks2)
-           (select-keys m2 ks2-ks1)
-           (select-keys m1
-                        (remove (fn [k] (= (m1 k) (m2 k)))
-                                ks1*ks2)))))
-
-
 (defn left-join-maps 
-  "Lamely simulate a sql left join on lmap and rmap using keycol to 
-   match data from either side"
+  "Lamely simulate a sql left join on two sequences of maps."
   [lmap rmap keycol]
   (pmap 
     (fn [m] (let [res (filter #(= (% keycol) (m keycol)) rmap)]
              (if (empty? res)
               (merge-with-empty m (first rmap))
-              (map-difference m (first res))))) lmap))
+              (merge m (first res))))) lmap))
 
 (defn create-unique-id
 	"Add a unique identifier with the name keycol
